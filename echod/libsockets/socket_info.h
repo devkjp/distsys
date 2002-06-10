579977374d9f0f270eaab73f138f13b91de84a78
rdrcode <rdrcode@users.noreply.github.com>
2002-06-10 16:48:19 +0000

    Initial revision

---
+++ echod/libsockets/socket_info.h
@@ -0,0 +1,24 @@
+
+#ifndef _SOCKET_INFO_H
+#define _SOCKET_INFO_H
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <string.h>
+
+struct socket_info {
+  char name[100];
+  char addr[20];
+  int port;
+};
+
+void get_socket_info(struct sockaddr_in from_sa, struct socket_info *si);
+
+int get_socket_name(int fd, struct socket_info *si);
+
+int get_socket_peer(int fd, struct socket_info *si);
+
+#endif
